e51cdce7bae4c9682a31be46ac789650be17bae0
Kian <Kian.berg333@gmail.com>
2024-06-02 15:19:14 -0600

    Create grid-generating function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 const gridhouse = document.querySelector("#gridhouse");
 gridhouse.style.backgroundColor = "white";
+/*
+Two test tiles below vvvv
 
 const tile1 = document.createElement("div");
 const tile2 = document.createElement("div");
@@ -9,3 +11,14 @@ tile2.classList.toggle("tile");
 
 gridhouse.appendChild(tile1);
 gridhouse.appendChild(tile2);
+*/
+
+function addGrid(gridSize) {
+    for(let gridCounter = 0; gridCounter < gridSize; gridCounter++) {
+        let div = document.createElement("div");
+        div.classList.toggle("tile");
+        gridhouse.appendChild(div);
+    }
+}
+
+addGrid(15);
