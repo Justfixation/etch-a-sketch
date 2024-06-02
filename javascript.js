883013f6ed35e175c39d4414239ad00b9e4b2796
Kian <Kian.berg333@gmail.com>
2024-06-02 17:01:20 -0600

    Add color change when tile is hovered

--- javascript.js
+++ javascript.js
@@ -1,24 +1,14 @@
 const gridhouse = document.querySelector("#gridhouse");
-gridhouse.style.backgroundColor = "white";
-/*
-Two test tiles below vvvv
-
-const tile1 = document.createElement("div");
-const tile2 = document.createElement("div");
-
-tile1.classList.toggle("tile");
-tile2.classList.toggle("tile");
-
-gridhouse.appendChild(tile1);
-gridhouse.appendChild(tile2);
-*/
 
 function addGrid(gridSize) {
     for(let gridCounter = 0; gridCounter < gridSize ** 2; gridCounter++) {
         let div = document.createElement("div");
         div.classList.toggle("tile");
         div.style.cssText = (`min-width: ${100/gridSize}%; min-height: ${100/gridSize}%`);
+        div.addEventListener("mouseenter", () => {
+            div.classList.add("coloredTile");
+        });
         gridhouse.appendChild(div);
     }
 }
-addGrid(100);
+addGrid(50);
