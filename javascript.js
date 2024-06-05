d955ad6f1e9eb8039caa54cd3822732b0e888e81
Kian <Kian.berg333@gmail.com>
2024-06-04 23:48:12 -0600

    Add darkening effect each time tile is moused over
    Darkness takes 10 hovers to fully fade black.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-const gridhouse = document.querySelector("#gridhouse");
+const gridHouse = document.querySelector("#gridHouse");
 const resetBtn = document.querySelector(".reset");
 
 function addGridWidth(gridWidth) {
@@ -7,27 +7,34 @@ function addGridWidth(gridWidth) {
         div.classList.add("tile");
         div.style.cssText = (`min-width: ${100/gridWidth}%; min-height: ${100/gridWidth}%`);
         /*Changes tile size to fit with other tiles */
+        let darkness = 1;
         div.addEventListener("mouseenter", () => {
             div.classList.add("coloredTile");
+            div.style.opacity = `${darkness}`;
+            if(darkness >= 0) {
+                darkness -= 0.1;
+            }
         });
-        /* Changes tile to blue when hovered */
-        gridhouse.appendChild(div);
+        /* Changes tile to blue when hovered, and 10% closer to black every time after */
+        gridHouse.appendChild(div);
     }
 }
 
 let resetMsg = "Enter a width from 1-100";
 function resetGrid() {
-    while(gridhouse.firstChild) {
-        gridhouse.removeChild(gridhouse.lastChild);
+    while(gridHouse.firstChild) {
+        gridHouse.removeChild(gridHouse.lastChild);
     }
     /* Removes child tiles if any exist*/
     let gridWidth = prompt(resetMsg);
     if(typeof(parseInt(gridWidth)) == "number" && gridWidth <= 100 && gridWidth > 0) {
         resetMsg = "Enter a width from 1-100";
         resetBtn.textContent = "Reset/Resize";
+        gridHouse.style.backgroundColor = "black";
         addGridWidth(gridWidth);
     } else if(gridWidth == null || gridWidth == 0) {
         resetBtn.textContent = "Click to add a sketch grid";
+        gridHouse.style.backgroundColor = "white";
         /*Resets to square 1 if user backs out (since grid is now cleared)*/
     } else {
         resetMsg = "The grid's width can only be a number from 1-100.\nHow wide should the new grid be?";
